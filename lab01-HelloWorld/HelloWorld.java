c7ad140a2594e286b43967ee425c0c46453e85af
bobbystorer <rws216@lehigh.edu>
2015-08-27 15:44:26 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,25 @@
+//Robert W. Storer
+//CSE2 Lab 1 - Hellow World
+//Thursday, August 27, 2015
+
+//first, define a class (name should match file name)
+public class HelloWorld {
+    
+    //next, add a method
+    public static void main (String [] args) {
+        
+        
+        //Now, print the statement detailed in the lab manual
+        System.out.println("#####################");
+        System.out.println("#####################");
+        System.out.println("#                   #");
+        System.out.println("# Hello World 2015! #");
+        System.out.println("#                   #");
+        System.out.println("#####################");
+        System.out.println("#####################");
+        System.out.println("test");
+        
+        //When I run this program, it should print the text in green.
+    } //close main method
+} //close class
+    
