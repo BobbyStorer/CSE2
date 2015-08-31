c5dd89d9699922150bc7d900252e087f39db877e
bobbystorer <rws216@lehigh.edu>
2015-08-31 13:57:46 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+//Robert W. Storer
+//August 31, 2015
+//CSE2
+//Homework Assignment 1
+
+//create a class
+
+public class WelcomeClass {
+    
+    //add main method
+    
+    public static void main (String [] args) {
+        
+        //use print statements to have computer display desired text
+        
+        System.out.println("  -----------");
+        System.out.println("  | WELCOME |");
+        System.out.println("  -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^  ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-R--W--S--2--1--6-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v  ");
+        System.out.println("From August 18-21, my friends from home and I took a trip to upstate New York.");
+        System.out.println("We stayed on a 300 acre farm. It was a great way to end the summer break.");
+       
+
+        
+    }   //close main method
+} //close class
